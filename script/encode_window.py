5f35b67aab8d6819fbdffcc3d6d822b6264a6d97
Soumyadeep Roy <10988419+roysoumya@users.noreply.github.com>
2018-04-13 00:15:35 +0530

    Updated to work with new data_script.py

--- script/encode_window.py
+++ script/encode_window.py
@@ -3,6 +3,7 @@ import numpy as np
 import gensim, os
 import tensorflow as tf
 
+
 def create_document_iter(tokens):
     for doc in tokens:
         raw_doc = ""
@@ -10,12 +11,14 @@ def create_document_iter(tokens):
             raw_doc += " " + word
         yield raw_doc.strip()
 
-def encode_dictionary(input_iter, min_frequence=0, max_document_length=10000): 
+
+def encode_dictionary(input_iter, min_frequence=0, max_document_length=10000):
     vocab_processor = tf.contrib.learn.preprocessing.VocabularyProcessor(
         max_document_length,
         min_frequence)
     vocab_processor.fit(input_iter)
     return vocab_processor
+
 
 def encode_window(tokens, anchors, vocab_processor):
     windows, window, labels = [], [], []
@@ -30,11 +33,12 @@ def encode_window(tokens, anchors, vocab_processor):
                     window.append(vocab_processor.vocabulary_._mapping.get(doc[i + tok], unk))
             windows.append(window)
             labels.append(anchors[j][tok])
-            #if anchors[j][tok] == 0: labels.append(0)
-            #else: labels.append(1)
+            # if anchors[j][tok] == 0: labels.append(0)
+            # else: labels.append(1)
             window = []
         j += 1
     return windows, labels
+
 
 def load_bin_vec(fname, vocab):
     """
@@ -42,16 +46,19 @@ def load_bin_vec(fname, vocab):
         """
     word_vecs = np.zeros((len(vocab), 300))
     count = 0
-    vocab_bin = gensim.models.word2vec.Word2Vec.load_word2vec_format(
-        os.path.join(os.path.dirname(__file__), fname), binary=True)
+    # DeprecationWarning . Use gensim.models.KeyedVectors.load_word2vec_format
+    # vocab_bin = gensim.models.word2vec.Word2Vec.load_word2vec_format(os.path.join(os.path.dirname(__file__), fname), binary=True)
+    vocab_bin = gensim.models.KeyedVectors.load_word2vec_format(os.path.join(os.path.dirname(__file__), fname), binary=True)
+
     for word in vocab:
         if word in vocab_bin:
             count += 1
-            word_vecs[vocab.index(word)]=(vocab_bin[word])
+            word_vecs[vocab.index(word)] = (vocab_bin[word])
         else:
             word_vecs[vocab.index(word)] = (np.random.uniform(-0.25, 0.25, 300))
-        print("found %d" %count)
+        print("found %d" % count)
     return word_vecs
+
 
 def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
@@ -60,26 +67,28 @@ def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
     for word in vocab:
         if word not in word_vecs and vocab[word] >= min_df:
-            word_vecs[word] = np.random.uniform(-0.25,0.25,k)
+            word_vecs[word] = np.random.uniform(-0.25, 0.25, k)
+
 
 if __name__ == "__main__":
     tokens1 = pickle.load(open("../tokens1.bin", "rb"))
-    tokens2 = pickle.load(open("../tokens2.bin", "rb"))
-    anchors1 = pickle.load(open("anchors1.bin", "rb"))
-    anchors2 = pickle.load(open("../anchors2.bin", "rb"))
-    input_iter = create_document_iter(tokens1 + tokens2)
+    #tokens2 = pickle.load(open("../tokens2.bin", "rb"))
+    anchors1 = pickle.load(open("../anchors1.bin", "rb"))
+    #anchors2 = pickle.load(open("../anchors2.bin", "rb"))
+    #input_iter = create_document_iter(tokens1 + tokens2)
+    input_iter = create_document_iter(tokens1)
     vocab = encode_dictionary(input_iter)
-    
+
     vocab_list = list(vocab.vocabulary_._mapping.keys())
     word_vecs = load_bin_vec("GoogleNews-vectors-negative300.bin", vocab_list)
     pickle.dump(word_vecs, open("../vector.bin", "wb"))
     del word_vecs
     windows1, labels1 = encode_window(tokens1, anchors1, vocab)
-    windows2, labels2 = encode_window(tokens2, anchors2, vocab)    
+    #windows2, labels2 = encode_window(tokens2, anchors2, vocab)
     pickle.dump(windows1, open("../windows1.bin", "wb"))
     pickle.dump(labels1, open("../labels1.bin", "wb"))
-    pickle.dump(windows2, open("../windows2.bin", "wb"))
-    pickle.dump(labels2, open("../labels2.bin", "wb"))
+    #pickle.dump(windows2, open("../windows2.bin", "wb"))
+    #pickle.dump(labels2, open("../labels2.bin", "wb"))
 
     """windows, labels = encode_window(tokens, anchors, dictionary)
     pickle.dump(windows, open("windows2.bin", "wb"))
